ea0294cbb0e7dfaf1506c338944ab9457b303c9b
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2026-04-04 13:44:21 -0500

    Same thing again?

--- js/WorkComponent.js
+++ js/WorkComponent.js
@@ -1,8 +1,10 @@
 import ResumeModel from "./ResumeModel.js"
 
-export default function WorkComponent () { return ({
+function WorkComponent () { return ({
     view: () => {
         return m("h1", ResumeModel.basics.name)
     }
 })
 }
+
+export default WorkComponent
