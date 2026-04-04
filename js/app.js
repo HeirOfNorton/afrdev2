af2a9bcb2000ce2362d67b1adfdae82891a7350c
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2026-04-04 13:59:23 -0500

    Okay, putting them all in one file, to see if it's the components or the imports that are messing it up.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 // import * as m from "./mithril.min.js";
 
-import WorkComponent from "./WorkComponent.js"
+//import WorkComponent from "./WorkComponent.js"
+
+
+
+const ResumeModel = {
+    basics: {
+        name: "Test Name",
+    }
+}
+
+const WorkComponent = {
+    view: () => {
+        return m("h1", ResumeModel.basics.name)
+    }
+}
 
 const MainView = {
     view: () => m(".container", [WorkComponent] )
