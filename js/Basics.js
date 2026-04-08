bfff77759a208d882110f317d93441a021eb8d89
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2026-04-08 11:39:41 -0500

    Wrapped Basics form in an article for "card" styling

--- js/Basics.js
+++ js/Basics.js
@@ -6,23 +6,25 @@ function Basics () {
     return {
         view: (vnode) => m("form",
             m("h1", "Basic Information"),
-            m("fieldset",
-                m("label",
-                    "Name",
-                    m("input[type=text][name=Name][placeholder=Full Name]")
-                ),
-                m("label",
-                    "Address",
-                    m("input[type=text][name=address1][placeholder=123 Anywhere Lane]"),
-                    m("input[type=text][name=address2]")
-                ),
-                m("label",
-                    "Phone Number",
-                    m("input[type=tel][name=phone]")
-                ),
-                m("label",
-                    "Email",
-                    m("input[type=email][name=email][placeholder=yourname@example.com]")
+            m("article",
+                m("fieldset",
+                    m("label",
+                        "Name",
+                        m("input[type=text][name=Name][placeholder=Full Name]")
+                    ),
+                    m("label",
+                        "Address",
+                        m("input[type=text][name=address1][placeholder=123 Anywhere Lane]"),
+                        m("input[type=text][name=address2]")
+                    ),
+                    m("label",
+                        "Phone Number",
+                        m("input[type=tel][name=phone]")
+                    ),
+                    m("label",
+                        "Email",
+                        m("input[type=email][name=email][placeholder=yourname@example.com]")
+                    )
                 )
             )
         )
