5d352fc0a95ae0319ac1a1e028c50079bf44d478
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2026-04-09 15:46:09 -0500

    added skeleton for Navigator

---
+++ js/Navigator.js
@@ -0,0 +1,23 @@
+import ResumeModel from "./ResumeModel"
+
+
+
+const Navigator = {
+    getNext: () => {
+
+    },
+    getLast: () => {
+
+    },
+    getSkip: () => {
+
+    },
+    getPageOrderList: () => {
+
+    },
+    setSkip: (page) => {
+
+    },
+}
+
+export default Navigator
