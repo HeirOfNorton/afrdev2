f536247b4af377678d808201e0b794e26b9952b1
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2026-04-14 15:02:17 -0500

    starting the work section

--- js/Work.js
+++ js/Work.js
@@ -1,5 +1,14 @@
 import ResumeModel from "./ResumeModel.js"
 
+const blankWorkItem = {
+    position: "",
+    organization: "",
+    location: "",
+    startDate: "",
+    endDate: "",
+    highlights: []
+}
+
 const Work = {
     view: () => {
         return m("h1", "Placeholder")
