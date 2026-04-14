4045733c2c7c20ab7e6aa1a96b0ca18446f39bb8
HeirOfNorton <2151484+HeirOfNorton@users.noreply.github.com>
2026-04-14 15:46:16 -0500

    added skeleton of work experience page

--- js/Work.js
+++ js/Work.js
@@ -1,4 +1,5 @@
 import ResumeModel from "./ResumeModel.js"
+import Navigator from "./Navigator.js"
 
 const blankWorkItem = {
     position: "",
@@ -10,9 +11,41 @@ const blankWorkItem = {
 }
 
 const Work = {
-    view: () => {
-        return m("h1", "Placeholder")
-    }
+    view: (vnode) => m("section",
+        m("h1", "Work Experience"),
+        m("table",
+            ResumeModel.data.work.map((item, index) => m("tr",
+                m("td",
+                    m("h3", item.position),
+                    m("h2", item.organization),
+                    m("p",
+                        Date.parse(item.startDate).toLocaleString(options={month:"long", year:"numeric"}),
+                        " &ndash; ",
+                        item.endDate == "current" ? "current" : 
+                            Date.parse(item.endDate).toLocaleString(options={month:"long", year:"numeric"})
+                    )
+                ),
+                m("td", m("button", "Edit"))
+            )),
+            m("tr",
+                m("td"),
+                m("td", m("button", "Add"))
+            )
+        ),
+        m("footer.flexx",
+            m(Navigator.BackButton, {href: Navigator.getLast()}),
+            m(m.route.Link,
+                {
+                    href: Navigator.getNext(),
+                    role: "button",
+                    onclick: e => {
+                        ResumeModel.saveToLocal()
+                    }
+                },
+                "Save and Continue"
+            )
+        )
+    )
 }
 
 export default Work
